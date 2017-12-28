b7ab7122e7aa4d25cadf680d55fdd0ed5f0f0b90
wzn <997402405@qq.com>
2017-12-28 16:22:56 +0800

    资产

--- src/main.js
+++ src/main.js
@@ -1,48 +1,15 @@
-/*
- * @Author: wangzunian 
- * @Date: 2017-04-07 11:45:04 
- * @Last Modified by: wangzunian
- * @Last Modified time: 2017-12-28 16:04:30
- */
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-import Vuex from 'vuex'
-import store from './store/index'
-import filter from './filter/filter'
-import validate from './validate/validate'
 
-/**  引入YDUI*/
-import YDUI from 'vue-ydui';
-import 'vue-ydui/dist/ydui.rem.css';
-
-Vue.use(Vuex);
-Vue.use(YDUI);
-
-/** 引入工具类函数 */
-import tool from './utils/tool'
-import api from './fetch/api'
-
-/**  vue项目配置 */
 Vue.config.productionTip = false
 
-
-/** 把一些公共方法或者属性注入到vue.prototype上面 */
-const addVueInstanceMethod = {
-  tool,
-  api
-}
-Object.assign(Vue.prototype, addVueInstanceMethod)
-
-/** 初始化入口函数 */
+/* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
-  store,
-  filter,
-  validate,
   template: '<App/>',
-  components: {
-    App
-  }
+  components: { App }
 })
--- src/router/index.js
+++ src/router/index.js
@@ -1,29 +1,15 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-const Hello = resolve => require(['../components/Hello.vue'], resolve)
-const Form = resolve => require(['../components/Form.vue'], resolve)
-const List = resolve => require(['../components/List.vue'], resolve)
+import HelloWorld from '@/components/HelloWorld'
+
 Vue.use(Router)
 
 export default new Router({
-  routes: [{
-      path: '/index',
-      name: 'Hello',
-      component: Hello
-    },
+  routes: [
     {
-      path: '/form',
-      name: 'Form',
-      component: Form
-    },
-    {
-      path: '/list',
-      name: 'List',
-      component: List
-    },
-    {
-      path: '',
-      redirect: '/form'
+      path: '/',
+      name: 'HelloWorld',
+      component: HelloWorld
     }
   ]
 })
